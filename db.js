00354baaba4143edb2195aabbb354ec22e583fea
quoccuong021099 <quoccuong021099@gmail.com>
2021-12-21 10:29:56 +0700

    a

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-  const conStr = 'mongodb://localhost:27017/authentication';
-  const conn = await mongoose.connect(conStr);
-  console.log(`DB connection successful: ${conn.connection.host}`);
-};
-
-module.exports = connectDB;
